00f6612047f96f9856a13871ac4f06ffce4fbd4e
stevehunt70 <steve@infineo-solutions.co.uk>
2026-05-21 16:44:45 +0100

    App.jsx update 2

--- src/App.jsx
+++ src/App.jsx
@@ -32,14 +32,15 @@ const AuthenticatedApp = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<Landing />} />
       <Route element={<AppLayout />}>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route index element={<Dashboard />} />
+        <Route path="/welcome" element={<Landing />} />
         <Route path="/generate" element={<GenerateQuiz />} />
         <Route path="/history" element={<QuizHistory />} />
         <Route path="/artist-lookup" element={<ArtistLookup />} />
       </Route>
-      <Route path="*" element={<PageNotFound />} />
+
+        <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 };
