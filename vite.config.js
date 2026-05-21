095c29e0a4329c45043b2acd8b8a61ac84544614
stevehunt70 <steve@infineo-solutions.co.uk>
2026-05-21 16:21:08 +0100

    vite.config '@' alias added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-//TEST: REender should see this
+//TEST: Render should see this
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   logLevel: 'error',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     react(),
   ],
